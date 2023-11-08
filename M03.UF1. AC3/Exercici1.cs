7e0de70a0adc09ee808b74deb667d580b2c0e83a
jordisanchoitb <jordi.sancho.7e7@itb.cat>
2023-11-08 16:59:09 +0100

    Fet exercicis AC3 i Repte2 fet

---
+++ M03.UF1. AC3/Exercici1.cs
@@ -0,0 +1,41 @@
+﻿/*
+ * Author: Jordi Sancho Garcia
+ * M03. Programació UF1
+ * v1. 8/11/2023
+ * Exercici 1. Implementa un programa que demani a l’usuari quants valors vol introduir i
+ * el programa generi una llista, a partir de valors introduïts per teclat, retornant 
+ * la suma de tots els elements d’aquesta.
+ *
+ */
+using System;
+
+namespace Exercici1
+{
+    public class Exercici1
+    {
+        public static void Main()
+        {
+            const string MSGProgram = "Donem una serie de numeros i et retorno la suma de tots ells."; 
+            const string MSGCase = "Quant valors vols introduir?";
+            const string MSGSumNumbers = "La suma del nombres introduit es: ";
+            int userinputmaxnumbers, usernumber, sumnumbers=0;
+
+            Console.WriteLine(MSGProgram);
+            Console.WriteLine(MSGCase);
+            userinputmaxnumbers = Convert.ToInt32(Console.ReadLine());
+            int[] arrayUserNumbers = new int[userinputmaxnumbers];
+            for (int i = 0; i < userinputmaxnumbers; i++)
+            {
+                usernumber = Convert.ToInt32(Console.ReadLine());
+                arrayUserNumbers[i] = usernumber;
+            }
+            for (int i = 0;i < arrayUserNumbers.Length; i++) 
+            { 
+                sumnumbers += arrayUserNumbers[i]; 
+            }
+            Console.WriteLine($"{MSGSumNumbers}{sumnumbers}");
+
+        }
+    }
+}
+
---
+++ Reptes/Repte 2 - Piràmide quadrada/Repte2.cs
@@ -0,0 +1,42 @@
+﻿/*
+ * Author: Jordi Sancho Garcia
+ * M03. Programació UF1
+ * v1. 18/10/2023
+ * Rept 2.  La torre ha de ser en forma de piràmide quadrada.Això significa que el primer pis tindrà un Ferrero, 
+ * el segon 4, el tercer 9, el quart 16, i així successivament (cada pis és un quadrat amb un més a cada cantó).
+ *
+ */
+using System;
+
+namespace Rept2
+{
+    public class Rept2
+    {
+        public static void Main()
+        {
+            const string MSGProgram = "Donem el numero de pisos i et retorno quants bombons necesites."; 
+            const string MSGCase = "Quants casos vols provar?";
+            const string MSGGetNmbers = "Introdueix el numero de pisos: ";
+            const string MSGNumberBombon = "El numero de bombons necesaris es: ";
+
+            int userinputcase, usernumber, numberbombon;
+
+            Console.WriteLine(MSGProgram);
+            Console.WriteLine(MSGCase);
+            userinputcase = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < userinputcase; i++)
+            {
+                numberbombon = 0;
+                Console.WriteLine(MSGGetNmbers);
+                usernumber = Convert.ToInt32(Console.ReadLine());
+                for (int j = 0; j <= usernumber;j++)
+                {
+                    numberbombon += j * j;
+                }
+                Console.WriteLine($"{MSGNumberBombon}{numberbombon}");
+            }
+
+        }
+    }
+}
+
