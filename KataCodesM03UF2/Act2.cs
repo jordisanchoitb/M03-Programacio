0de5f780dac93b3ccae358e1b19638847e2126e5
jordisanchoitb <jordi.sancho.7e7@itb.cat>
2023-12-19 11:55:54 +0100

    Act katacodesM03UF2

---
+++ KataCodesM03UF2/Act2.cs
@@ -0,0 +1,35 @@
+﻿/*
+ * Author: Jordi Sancho Garcia
+ * M03. Programació UF2
+ * v1. 19/12/2023
+ * 2. Implementa un mètode que retorni el valor absolut de l'argument rebut.
+ * NO es pot fer servir la classe Math.
+ */
+
+using System;
+
+namespace Ex2
+{
+    public class Ex2
+    {
+        public static void Main()
+        {
+            const string MsgProgram = "Introdueix un numero i et retorno el seu valor absolut";
+            const string MsgResult = "El nombre absolut es: {0}";
+            int usernumber;
+            Console.WriteLine(MsgProgram);
+            usernumber = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(MsgResult, AbsoluteValue(usernumber));
+        }
+
+        public static int AbsoluteValue(int usernumber)
+        {
+            if (usernumber < 0)
+            {
+                usernumber = usernumber * -1;
+            }
+            return usernumber;
+        }
+    }
+}
---
+++ KataCodesM03UF2/Act4.cs
@@ -0,0 +1,44 @@
+﻿/*
+ * Author: Jordi Sancho Garcia
+ * M03. Programació UF2
+ * v1. 19/12/2023
+ * 4. Implementa el mètode anterior però en aquest cas haurà de validar 
+ * si el valor és dins del rang de valors introduïts per teclat 
+ */
+
+using System;
+
+namespace Ex4
+{
+    public class Ex4
+    {
+        public static void Main()
+        {
+            const string MsgProgram = "Introdueix un numero que vols comprovar si esta dins del rang:";
+            const string MsgMinRang = "Introdueix el valor minim del rang:";
+            const string MsgMaxRang = "Introdueix el valor maxim del rang:";
+            const string MsgIsInRange = "El numero introduit esta dins del rang";
+            const string MsgIsNotInRange = "El numero introduit esta fora del rang";
+
+            int usernumber, maxrang, minrang;
+            Console.WriteLine(MsgProgram);
+            usernumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(MsgMinRang);
+            minrang = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(MsgMaxRang);
+            maxrang = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(IsInRange(usernumber, maxrang, minrang) ? MsgIsInRange : MsgIsNotInRange);
+            
+        }
+        
+        public static bool IsInRange(int usernumber, int maxrang, int minrang)
+        {
+            if (usernumber < maxrang && usernumber >= minrang)
+            {
+                 return true;
+            }
+            return false;
+        }
+    }
+}
